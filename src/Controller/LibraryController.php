615893c745b648fe6f32be804b3ff08fc06cff93
Andi Dupa <andidupa@bth.se>
2025-05-13 20:07:35 +0200

    Working on kmom05: Finishing Bibliotek CRUD

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -83,4 +83,79 @@ final class LibraryController extends AbstractController
 
         return $this->render('library/read.html.twig', $data);
     }
+
+    #[Route('/library/update_edit', name: 'library_update_landing', methods: ['POST'])]
+    public function updateBookLanding(
+        ManagerRegistry $doctrine,
+        BooksRepository $booksRepository,
+        Request $request
+    ): Response {
+        $id = $request->request->get("book_id");
+
+        $books = $booksRepository
+        ->find($id);
+
+        $book = [
+            $books
+        ];
+
+        $data = [
+            "books" => $book,
+        ];
+
+        return $this->render('library/update.html.twig', $data);
+    }
+
+    #[Route('/library/update', name: 'library_update', methods: ['POST'])]
+    public function updateBook(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response {
+        $id = $request->request->get("book_id");
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $title = $request->request->get('title');
+        $author = $request->request->get('author');
+        $isbn = $request->request->get('isbn');
+        $image = $request->request->get('image');
+
+        $book->setTitle($title);
+        $book->setAuthor($author);
+        $book->setISBN($isbn);
+        $book->setImage($image);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_read_many');
+    }
+
+    #[Route('/library/delete', name: 'library_delete_by_id', methods: ["POST"])]
+    public function deleteBookById(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response {
+        $id = $request->request->get("book_id");
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_read_many');
+    }
 }
